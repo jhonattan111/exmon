3ffeaaf00bb5eece32ba217740d7f50c2d2de665
Jhonattan <jhonattan10alves@gmail.com>
2022-07-11 00:21:52 -0300

    subindo repo

---
+++ lib/exmon.ex
@@ -0,0 +1,18 @@
+defmodule Exmon do
+  @moduledoc """
+  Documentation for `Exmon`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exmon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
